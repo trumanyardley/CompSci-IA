02a28aaef7bf5204d24099a1641e4b66423b4bb4
tyardley03 <truman.yardley@gmail.com>
2020-04-22 13:55:19 -0400

    unecessary imports removed

--- Graph.java
+++ Graph.java
@@ -1,7 +1,6 @@
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.Graphics.*;
 import java.util.ArrayList;
 import java.awt.event.*;
 
--- GraphPanel.java
+++ GraphPanel.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.Graphics.*;
 import java.util.ArrayList;
 
 /*
